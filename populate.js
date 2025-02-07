184286a7abd413118029f8f3a485d510f1307144
shreya <shreya.bhushan2021@vitstudent.ac.in>
2025-02-08 00:05:48 +0530

    update3

--- populate.js
+++ populate.js
@@ -1,24 +1,51 @@
-import { readFile } from "fs/promises";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import { readFile } from 'fs/promises';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 dotenv.config();
 
-import Job from "./models/JobModel.js";
-import User from "./models/UserModel.js";
+import Job from './models/JobModel.js';
+import User from './models/UserModel.js';
 try {
-  await mongoose.connect(process.env.MONGO_URL);
-  const user = await User.findOne({ email: "test@test.com" });
-  const jsonJobs = JSON.parse(
-    await readFile(new URL("./utils/mockData.json", import.meta.url))
-  );
-  const jobs = jsonJobs.map((job) => {
-    return { ...job, createdBy: user._id };
-  });
-  await Job.deleteMany({ createdBy: user._id });
-  await Job.create(jobs);
-  console.log("success");
-  process.exit(0);
+    await mongoose.connect(process.env.MONGO_URL);
+    const user=await User.findOne({email:'shreya@gmail.com'});
+    const jsonJobs=JSON.parse(
+        await readFile(new URL('./utils/mockData.json',import.meta.url))
+    );
+    const jobs=jsonJobs.map((job)=>{
+        return {...job,createdBy:user._id};
+    });
+    await Job.deleteMany({createdBy:user._id});
+    await Job.create(jobs);
+    console.log("success");
+    process.exit(0);
 } catch (error) {
-  console.log(error);
-  process.exit(1);
+    console.log(error);
+    process.exit(1);
 }
+
+
+
+// import { readFile } from "fs/promises";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// import Job from "./models/JobModel.js";
+// import User from "./models/UserModel.js";
+// try {
+//   await mongoose.connect(process.env.MONGO_URL);
+//   const user = await User.findOne({ email: "test@test.com" });
+//   const jsonJobs = JSON.parse(
+//     await readFile(new URL("./utils/mockData.json", import.meta.url))
+//   );
+//   const jobs = jsonJobs.map((job) => {
+//     return { ...job, createdBy: user._id };
+//   });
+//   await Job.deleteMany({ createdBy: user._id });
+//   await Job.create(jobs);
+//   console.log("success");
+//   process.exit(0);
+// } catch (error) {
+//   console.log(error);
+//   process.exit(1);
+// }
